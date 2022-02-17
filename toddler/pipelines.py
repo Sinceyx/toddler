14f243ffd5a590ab5029333a6a20b240dd58775b
Xin.Yu <sinceyx@outlook.com>
2022-02-17 10:53:07 +0800

    add filter for pipelines

--- toddler/pipelines.py
+++ toddler/pipelines.py
@@ -6,6 +6,7 @@
 
 # useful for handling different item types with a single interface
 import json
+import re
 
 from toddler.items import HouseInfoItem
 
@@ -21,8 +22,24 @@ class ToddlerPipeline:
     def close_spider(self, spider):
         self.file.close()
 
+    def __state_name_filter(self, full_address: str):
+        state_name_reject_list = ['NSW']
+        state_name = re.compile('([A-Z]{2,})[\s]+[0-9]{4}$').search(full_address).groups(1)[0]
+        if state_name not in state_name_reject_list:
+            return True
+
+    def __listing_type_filter(self, listing_type: str):
+        listing_type_access_list = ['FOR SALE', 'FOR LEASE']
+        if listing_type.upper() in listing_type_access_list:
+            return True
+
+    def __to_filter(self, item):
+        return self.__listing_type_filter(item['listing_type']) and self.__state_name_filter(item['full_address'])
+
     def process_item(self, item, spider):
         if isinstance(item, HouseInfoItem):
-            line = json.dumps(dict(item)) + '\n'
-            self.file.write(line)
-        return item
+            if self.__to_filter(self, item):
+                self.file.write(json.dumps(dict(item)) + '\n')
+                return item
+
+
