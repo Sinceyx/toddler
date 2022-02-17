a4c0af77003255ccf20ec32abceb0e142b4efc34
Xin.Yu <sinceyx@outlook.com>
2022-02-17 11:03:53 +0800

    init test_luton_spider.py

---
+++ toddler/spiders/test_luton_spider.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class LutonSpiderTest(unittest.TestCase):
+	def test_parse(self):
+		self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+	unittest.main()
